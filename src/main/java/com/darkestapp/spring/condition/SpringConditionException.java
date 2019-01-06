3e2d946d8bc3599b8cbe27317728546445787311
Manuel <darkpriestrelative@gmail.com>
2019-01-06 12:28:34 -0300

    Implemented SpringConditionException.
    Fix #12

---
+++ src/main/java/com/darkestapp/spring/condition/SpringConditionException.java
@@ -0,0 +1,11 @@
+package com.darkestapp.spring.condition;
+
+/**
+ * Created by Manuel Perez P. (darkpriestrelative@gmail.com) on 06/01/19.
+ */
+public class SpringConditionException extends RuntimeException {
+
+    public SpringConditionException(String message) {
+        super(message);
+    }
+}
